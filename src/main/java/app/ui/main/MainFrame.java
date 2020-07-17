584191c0c50f3d9abdcd803e64dd6da1c4640b2d
JairoPacheco <48835989+JairoPacheco@users.noreply.github.com>
2020-07-17 11:19:36 -0600

    ui de motivo (#22)
    * ui de motivo
    
    * arreglos del request
    
    * Formato código y eliminar comments
    
    Co-authored-by: Fran Soto Q <fran-soto@hotmail.es>

--- src/main/java/app/ui/main/MainFrame.java
+++ src/main/java/app/ui/main/MainFrame.java
@@ -1,6 +1,7 @@
 package app.ui.main;
 
 import app.ui.client.list.ClientListFrame;
+import app.ui.reason.list.ReasonListFrame;
 import app.ui.expenseType.list.ExpenseTypeListFrame;
 import app.ui.taskType.list.TaskTypeListFrame;
 import app.ui.task.list.TaskListFrame;
@@ -237,7 +238,7 @@ public class MainFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_btnTaskTypeActionPerformed
 
     private void btnReasonsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReasonsActionPerformed
-
+        openFrame(new ReasonListFrame());
     }//GEN-LAST:event_btnReasonsActionPerformed
 
     private void btnSupportTypeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSupportTypeActionPerformed
